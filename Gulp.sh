c1b127bb302efe30b8d625d8fce4a2549f93f2e3
Filip Oščádal <filip@oscadal.com>
2019-03-06 23:01:19 +0100

    rewritten gulp installer, moved to Nodejs v11

--- Gulp.sh
+++ Gulp.sh
@@ -3,40 +3,67 @@
 dir="$(dirname "$0")"
 . $dir"/_includes.sh"
 
+info "Uninstall Node and Yarn?"
+yes_or_no && {
+  info "Removing Node and Yarn ..."
+  sudo apt-get remove -yq nodejs yarn
+  sudo rm -rf /usr/lib/node_modules
+}
+
 command -v nodejs >/dev/null 2>&1 || {
-  info "Installing Nodejs ..."
-  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  info "Installing Node ..."
+  curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
   sudo apt-get install -y nodejs
+  sudo npm rm --global gulp
 }
 
 command -v yarn >/dev/null 2>&1 || {
+  info "Installing Yarn ..."
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  sudo apt-get update
-  sudo apt-get install yarn
+  sudo apt-get update -qq
+  sudo apt-get install -y yarn
 }
 
-info 'Checking gulp ...'
-command -v gulp >/dev/null 2>&1 || {
-  info "Installing gulp ..."
-  sudo npm install --global gulp
-  npm link gulp
+echo -e "\n"
 
-  info 'Installing gulp plugins ...'
+v=`node --version`
+info "node version: "$v
+
+v=`npm --version`
+info "npm version: "$v
+
+v=`npx --version`
+info "npx version: "$v
+
+v=`yarn --version`
+info "yarn version: "$v
+
+echo -e "\n"
+
+info "Reinstall gulp?"
+yes_or_no && {
+  sudo npm rm --global gulp
+  sudo rm -f /usr/bin/gulp >/dev/null 2>&1
+  sudo npm install --global gulp-cli
+}
+
+info "Recreate project?"
+yes_or_no && {
+  npm init
+  npm install --save-dev gulp
+}
+
+info "Reinstall gulp plugins?"
+yes_or_no && {
+  rm -rf ./node_modules >/dev/null 2>&1
   npm install exec
-  npm install gulp
   npm install gulp-autoprefixer
   npm install gulp-concat
   npm install gulp-cssmin
-  npm install gulp-jshint
   npm install gulp-minify-css
   npm install gulp-rename
   npm install gulp-replace
   npm install gulp-uglify
   npm install gulp-util
 }
-
-info "Updating npm ..."
-X=`npm outdated -g --depth=0`
-if [ ! -z "$X" ]; then sudo npm update -g; fi
-npm update
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
-var gulp = require("gulp");
-var concat = require("gulp-concat");
+function defaultTask(cb) {
+  var gulp = require("gulp");
+  var concat = require("gulp-concat");
 
-gulp.task("default", function() {
-  gulp.src("www/js/gulp/*.js", {base: "www/js/gulp"})
+  gulp.src("www/js/gulp/*.js", {
+      base: "www/js/gulp"
+    })
     .pipe(concat("build.js"))
     .pipe(gulp.dest("www"));
-  gulp.src("www/css/gulp/*.css", {base: "www/css/gulp"})
+
+  gulp.src("www/css/gulp/*.css", {
+      base: "www/css/gulp"
+    })
     .pipe(concat("build.css"))
     .pipe(gulp.dest("www"));
-});
+
+    cb();
+}
+
+exports.default = defaultTask
