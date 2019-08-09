87bec8e8c6b82bc7864122b881119efa2eff82bf
Filip Oščádal <filip@oscadal.com>
2019-08-09 08:29:07 +0200

    monsterfuck icon update

--- www/img/create_favicons.sh
+++ www/img/create_favicons.sh
@@ -15,7 +15,7 @@ if ! [ -x "$(command -v convert)" ]; then
 fi
 
 if [ -f $INPUT_IMAGE ]; then
-  SIZES=(16 24 32 48 57 60 64 70 72 76 96 114 120 128 144 150 152 180 192 196 256 310 512)
+  SIZES=(16 24 29 32 40 48 57 58 60 64 70 72 76 80 87 96 114 120 128 144 150 152 167 180 192 196 256 310 320 512)
   for size in ${SIZES[@]}; do
     convert -flatten -background none -resize ${size}x${size} $INPUT_IMAGE $OUTPUT_DIR/favicon-${size}.png
     if [ -f favicon-${size}.png ]; then
