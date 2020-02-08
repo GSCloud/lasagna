928b84545577b470abe86554a3ef27a3a537f347
Filip Oščádal <f@mxd.cz>
2020-02-08 10:33:14 +0100

    robotic update

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,7 +11,7 @@ mkdir -p app cache ci data www/cdn-assets www/download www/upload
 chmod +x *.sh
 sudo chmod 0777 www/download www/upload
 sudo chmod -R 0775 cache ci data
-sudo chgrp www-data cache ci data www/cdn-assets www/download www/upload
+sudo chgrp -R www-data cache ci data www www/cdn-assets www/download www/upload
 sudo apt install php7.3-cli php7.3-curl php7.3-mbstring php7.3-mysql php7.3-sqlite3 php7.3-zip
 
 command -v composer >/dev/null 2>&1 || fail "PHP composer is not installed!"
