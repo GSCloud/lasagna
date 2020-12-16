124c774ae19c25aa8a020a53160476011f6e7b0f
Filip Oščádal <f@mxd.cz>
2020-12-16 13:54:29 +0100

    web sync

--- app/ArticlePresenter.php
+++ app/ArticlePresenter.php
@@ -24,30 +24,43 @@ class ArticlePresenter extends APresenter
      */
     public function process()
     {
-        // basic setup
-        $data = $this->getData();
+        // get current Presenter and View
         $presenter = $this->getPresenter();
         $view = $this->getView();
-        $this->checkRateLimit()->setHeaderHtml()->dataExpander($data); // Model
 
-        // advanced caching
-        $use_cache = (DEBUG === true) ? false : $data["use_cache"] ?? false;
+        // process rate limiting + set HTML header + expand current data model
+        $data = $this->getData();
+        $this->checkRateLimit()->setHeaderHtml()->dataExpander($data);
+
+        // process advanced caching
+        $use_cache = (bool) (DEBUG ? false : $data["use_cache"] ?? false);
         $cache_key = hash("sha256", join("_", [$data["host"], $data["request_path"], "htmlpage"]));
         if ($use_cache && $output = Cache::read($cache_key, "page")) {
             return $this->setData("output", $output .= "\n<script>console.log('*** page content cached');</script>");
         }
 
-        // fix locale text
-        foreach ($data["l"] ??= [] as $k => $v) {
-            StringFilters::correct_text_spacing($data["l"][$k], $data["lang"]);
+        // fix current locale
+        foreach ($data["l"]??=[] as $k => $v) {
+            StringFilters::convert_eolhyphen_to_brdot($data["l"][$k]);
+            StringFilters::convert_eol_to_br($data["l"][$k]);
+            StringFilters::correct_text_spacing($data["l"][$k], $data["lang"] ?? "en");
         }
 
-        $data["container_switch_article"] = true; // turn ON article view in content switcher
+        // turn ON article view in content switcher
+        $data["container_switch_article"] = true;
 
         // output rendering
         $output = $this->setData($data)->renderHTML($presenter[$view]["template"]);
-        StringFilters::trim_html_comment($output); // strip comments
-        Cache::write($cache_key, $output, "page"); // cache
-        return $this->setData("output", $output); // save model
+
+        // strip comments
+        StringFilters::trim_html_comment($output);
+
+        // save to page cache
+        if ($use_cache) {
+            Cache::write($cache_key, $output, "page");
+        }
+
+        // save output to model
+        return $this->setData("output", $output);
     }
 }
