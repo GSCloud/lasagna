df8105b6ec62d8b03fcce383964b424150a77453
Filip Oščádal <f@mxd.cz>
2021-01-20 17:59:48 +0100

    + _correct_text_spacing_sk(); + new HTTP error codes

--- app/ErrorPresenter.php
+++ app/ErrorPresenter.php
@@ -24,6 +24,9 @@ class ErrorPresenter extends APresenter
         420 => "Enhance Your Calm",
         429 => "Too Many Requests",
         500 => "Internal Server Error",
+        502 => "Bad Gateway",
+        503 => "Service Unavailable",
+        504 => "Gateway Timeout",
     ];
 
     /**
