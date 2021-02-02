617c2a8dcadc86c88fe18d403303d62bae18f722
Filip Oščádal <f@mxd.cz>
2021-02-02 20:32:13 +0100

    minor update

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,7 +12,9 @@ mkdir -p ci data logs temp www/cdn-assets www/download www/upload
 sudo chmod 0777 ci data logs temp www/download www/upload
 sudo chown -R www-data:www-data data
 sudo chgrp -R www-data ci data www www/cdn-assets www/download www/upload
-sudo apt install php7.4-cli php7.4-curl php7.4-mbstring php7.4-zip
+sudo apt-get install -y libapache2-mod-php8.0 openssl php-imagick php-redis \
+  php8.0 php8.0-cli php8.0-curl php8.0-gd php8.0-intl php8.0-mbstring \
+  php8.0-mysql php8.0-readline php8.0-pgsql php8.0-sqlite3 php8.0-xml php8.0-zip
 
 command -v composer >/dev/null 2>&1 || fail "PHP composer is not installed!"
 
