ea6501c8e66daf3e2b3681f4744096130dfeaf50
Filip Oščádal <f@mxd.cz>
2021-02-04 14:39:33 +0100

    minor update

--- DOCKER.sh
+++ DOCKER.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# connect to the running container and run CSV updater
 docker exec -ti tesseract ./docker_updater.sh
+
+# connect to the running container and run bash
 docker exec -ti tesseract bash
