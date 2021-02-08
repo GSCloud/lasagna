9966182b54491c4f61d982b885c0f570fc541528
Filip Oščádal <f@mxd.cz>
2021-02-08 08:40:31 +0100

    web sync

--- www/js/gulp/5.main.js
+++ www/js/gulp/5.main.js
@@ -1,9 +1,9 @@
 window.GSC.loaderOff();
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
   if ("serviceWorker" in navigator) {
     console.log("Service worker...");
-    navigator.serviceWorker.register("/sw.js?" + Date.now()).then(function (registration) {
+    navigator.serviceWorker.register("/sw.js?" + Date.now()).then(function(registration) {
       console.log("ServiceWorker registration successful with scope: ", registration.scope);
     }, function (err) {
       console.log("ServiceWorker registration failed: ", err);
@@ -12,7 +12,7 @@ window.addEventListener("load", function () {
     console.log("Service worker is not supported!");
   }
   (function ($) {
-    $(function () {
+    $(function() {
       M.AutoInit();
       window.GSC.AutoInit();
     });
