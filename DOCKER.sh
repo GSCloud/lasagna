f73871edde4934c32712949ef204e18ac4289e67
Filip Oščádal <f@mxd.cz>
2021-02-08 16:48:36 +0100

    + disable REDIS by 0 port

--- DOCKER.sh
+++ DOCKER.sh
@@ -3,8 +3,8 @@
 # connect to the running container and run CSV updater
 #docker exec -t tesseract ./docker_updater.sh
 
+# connect to the running container and run bash
+#docker exec -ti tesseract bash
+
 # connect to the running container and run PHP CLI
 docker exec -t tesseract ./cli.sh doctor
-
-# connect to the running container and run bash
-docker exec -ti tesseract bash
