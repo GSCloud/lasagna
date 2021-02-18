70b06e1597f28b7509502785d5aa93a4f90ac781
Filip Oščádal <f@mxd.cz>
2021-02-18 05:39:37 +0100

    + Makefile refactorization

--- bin/update_docker.sh
+++ bin/update_docker.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+#@author Filip Oščádal <oscadal@gscloud.cz>
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+
+dir="$(dirname "$0")"
+. $dir"/_includes.sh"
 
 # connect to container and run CSV updater
+info Updating CSV data from Google ...
+
 docker exec tesseract ./docker_updater.sh
+
+# connect to container and run bash
+docker exec tesseract make
 
 # connect to container and run bash
 docker exec -ti tesseract bash
