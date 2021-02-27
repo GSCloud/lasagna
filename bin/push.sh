51f40afd0bbb44bfbc0c215b7911e0fdb2b4dfe9
Filip Oščádal <f@mxd.cz>
2021-02-27 12:28:21 +0100

    web sync

---
+++ bin/push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#@author Filip Oščádal <oscadal@gscloud.cz>
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+
+dir="$(dirname "$0")"
+. $dir"/_includes.sh"
+
+command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
+
+if [ ! -n $(id -Gn "$(whoami)" | grep -c "docker") ]
+    then if [ "$(id -u)" != "0" ]; then fail "Add yourself to the 'docker' group or run this script as root!"; fi
+fi
+
+[ ! -r ".env" ] && fail "Missing .env file!"
+export $(grep -v '^#' .env | xargs -d '\n')
+
+[ -z "$TAG" ] && fail "Missing TAG definition!"
+docker push $TAG
