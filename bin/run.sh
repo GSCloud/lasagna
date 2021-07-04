1ee28c1d62cdd265f1942e1b0eeccafda7a9ad49
Filip Oščádal <f@mxd.cz>
2021-07-04 15:35:57 +0200

    fixing CONST exposure and private config mapping for container RUN

--- bin/run.sh
+++ bin/run.sh
@@ -18,8 +18,6 @@ source .env
 [ -z "$TAG" ] && fail "Missing TAG definition!"
 
 command -v google-chrome >/dev/null 2>&1 && google-chrome http://localhost:$PORT &
-
-#docker run --rm --name $NAME -p $PORT:80 -v "$(pwd)"/www/:/var/www/html/ -v "$(pwd)"/app/:/var/www/app/ $TAG
-docker run --rm --name $NAME -p $PORT:80 $TAG
+docker run --rm --name $NAME -p $PORT:80 -v $(pwd)/app/config_private.neon:/var/www/app/config_private.neon $TAG
 
 exit 0
