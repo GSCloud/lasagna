0e6c493a270d674da0393e0fc20e993546f2b677
Filip Oščádal <f@mxd.cz>
2022-04-24 12:08:16 +0200

    minor update

--- bin/create_pdf.sh
+++ bin/create_pdf.sh
@@ -6,13 +6,13 @@ dir="$(dirname "$0")"
 
 command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
 
-# MarkDown -> ADOC
-find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.md" \
+# MarkDown -> ADOC on files modified in the last 24 hours
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -mtime -1 -iname "*.md" \
     -exec echo "Converting {} to ADOC" \; \
     -exec docker run --rm -v "$(pwd)":/data pandoc/core:latest -f markdown -t asciidoc -i {} -o "{}.adoc" \;
 
-# ADOC -> PDF
-find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.adoc" \
+# ADOC -> PDF on files modified in the last 24 hours
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -mtime -1 -iname "*.adoc" \
     -exec echo "Converting {} to PDF" \; \
     -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor:latest asciidoctor-pdf "{}" \;
 
@@ -21,7 +21,9 @@ find . -maxdepth 1 -iname "*.adoc" -delete
 rm temp/* >/dev/null 2>&1
 
 # phpdocumentor
-docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/data phpdoc/phpdoc:3.1.0 \
+info "Removing old phpDoc files"
+sudo rm -rf doc/
+docker run --rm -v $(pwd):/data phpdoc/phpdoc:3.1.0 \
     run -d . -t ./doc --ignore "vendor/"
 
 # make link to documentation
