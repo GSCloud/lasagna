7bcb42b9978d5a166f3878e0133b9e52358abd21
Fred Brooker <git@gscloud.cz>
2025-12-29 19:27:55 +0100

    minor update

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#@author Fred Brooker <git@gscloud.cz>
+# @author Fred Brooker <git@gscloud.cz>
 
 info() {
-  echo -e " \e[1;32m*\e[0;1m ${*}\e[0m" 1>&2
+  echo -e "${*}\e[0m" 1>&2
 }
 
 warn() {
@@ -22,29 +22,59 @@ function yes_or_no () {
     case $yn in
       [Yy]*) return 0 ;;
       [Nn]*) return 1 ;;
-      *)
-      return 1 ;;
+      *) return 1 ;;
     esac
   done
 }
 
-info "Installing 👶"
+PACKAGES=(
+  libapache2-mod-php8.2 
+  openssl 
+  php8.2 
+  php8.2-cli 
+  php8.2-curl 
+  php8.2-gd 
+  php8.2-intl 
+  php8.2-mbstring 
+  php8.2-readline 
+  php8.2-xml 
+  php8.2-redis 
+  php8.2-imagick
+)
 
-mkdir -p ci data logs temp www/{download,upload} && find . -name "*.sh" -exec chmod +x {} +
+info "👶 installing"
 
-echo "Root permission needed to run some tasks 😎"
+# create directory structure for volumes
+mkdir -p ci custom data logs temp www/download/export www/upload www/img && find . -name "*.sh" -exec chmod +x {} +
 
-sudo chmod 0777 ci data logs temp www/{download,upload}
-sudo chown -R www-data:www-data data
-sudo chgrp -R www-data ci data www www/{download,upload}
+echo "😎 root permission needed to run the following tasks"
 
-yes_or_no "Run PHP 8.2 and Apache APT installation?" || exit 0
+# set permissions and ownership for all mapped volumes
+sudo chmod -R 0777 ci custom data logs temp www/download www/upload www/img
+sudo chown -R www-data:www-data custom data logs
+sudo chgrp -R www-data ci custom data logs www www/download www/upload www/img
 
-sudo apt-get install -yq libapache2-mod-php8.2 openssl php8.2 php8.2-cli \
-  php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-readline php8.2-xml php8.2-redis php8.2-imagick
-sudo a2enmod php8.2 expires headers rewrite
+# check for missing packages
+MISSING_PKGS=()
+for pkg in "${PACKAGES[@]}"; do
+    dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed" || MISSING_PKGS+=("$pkg")
+done
 
+if [ ${#MISSING_PKGS[@]} -eq 0 ]; then
+    info "✅ all PHP 8.2 components are already installed"
+else
+    warn "Missing packages: ${MISSING_PKGS[*]}"
+    if yes_or_no "Run PHP 8.2 and Apache APT installation?"; then
+        sudo apt-get update -yq
+        sudo apt-get install -yq "${PACKAGES[@]}"
+        sudo a2enmod php8.2 expires headers rewrite
+    else
+        warn "❌ installation skipped by user"
+    fi
+fi
+
+# check for composer and run update
 command -v composer >/dev/null 2>&1 || fail PHP composer is not installed!
-[ ! -d "vendor" ] &&  make update
+[ ! -d "vendor" ] && make update
 
-echo -en "\nRun \e[1m\e[4mmake doctor\e[0m to check your configuration 👨‍⚕️\n\n"
+info "👾 run \e[1m\e[4mmake doctor\e[0m to check your configuration\n\n"
